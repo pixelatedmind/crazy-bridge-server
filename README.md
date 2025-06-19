456313da70e3e3f3b341b95821c9b54b060e9681
Michael Arnaldo <mike.arnaldo@gmail.com>
2025-06-18 18:24:59 -0700

    Add files via upload
    add server code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# crazy-bridge-server
-Socket.io multiplayer backend for Crazy Bridge card game
+# Crazy Bridge Multiplayer Server
+
+This is a Socket.io + Express server for real-time multiplayer gameplay.
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+const rooms = {};
+
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("join", (roomId) => {
+    socket.join(roomId);
+    if (!rooms[roomId]) rooms[roomId] = [];
+    rooms[roomId].push(socket.id);
+    io.to(roomId).emit("players", rooms[roomId]);
+  });
+
+  socket.on("play_card", ({ roomId, card }) => {
+    socket.to(roomId).emit("card_played", { playerId: socket.id, card });
+  });
+
+  socket.on("disconnect", () => {
+    for (const roomId in rooms) {
+      rooms[roomId] = rooms[roomId].filter((id) => id !== socket.id);
+      io.to(roomId).emit("players", rooms[roomId]);
+    }
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
